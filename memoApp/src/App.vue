8f4cace52df6db3d3425098627765ac4b794fbeb
doitumura <1993nakayama@gmail.com>
2023-03-13 09:45:01 +0900

    不要なコンポーネント、cssを削除し、メモ追加、一覧機能を作成

--- memoApp/src/App.vue
+++ memoApp/src/App.vue
@@ -1,47 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+let id = 0;
+const newMemo = ref("");
+const memos = ref([]);
+
+const addMemo = () => {
+  memos.value.push({ id: id++, text: newMemo.value})
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <h1>メモアプリ</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+      <h2>メモを追加する</h2>
+      <form @submit.prevent="addMemo">
+        <input v-model="newMemo" />
+        <button>追加</button>
+      </form>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <h2 v-if="memos.length > 0">メモ一覧</h2>
+      <ul>
+        <li v-for="memo in memos" :key="memo.id">
+          <span>{{ memo.text }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  font-size: 70px;
 }
 </style>
